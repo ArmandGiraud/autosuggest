8c542b6d80960626a0b0b4a483c76dae41b9a995
Mathieu Havel <mhavel@users.noreply.github.com>
2019-03-25 17:31:55 +0100

    new `download.py`
    function `maybe_download` has been renamed to `maybe_download_queries` for more clarity on its purpose.
    Also, the function will now return the path of the queries file to be used.
    Downloading from an URL is optional but still possible of course.

--- autosuggest/download.py
+++ autosuggest/download.py
@@ -1,17 +1,37 @@
 """utilities to check data, annd download it"""
 
 import urllib.request as urllib
-import os
+from pathlib import Path
 
-SAVE_PATH = "data/data_processed_fixed.txt" # the maybe_download will be run from the __init__ in first level folder
-data_dir = os.path.abspath(os.path.join(__file__, os.pardir))
-SAVE_PATH = os.path.join(data_dir, SAVE_PATH)
+from .resources import get_resource
 
-def maybe_download(url):
-    print("Downloading {}".format(os.path.basename(SAVE_PATH)))
-    if not os.path.exists(SAVE_PATH):
-        file_path, _ = urllib.urlretrieve(url=url,
-                                            filename=SAVE_PATH)
 
-if __name__ == "__main__":
-    maybe_download(url = "secret url")
+def maybe_download_queries(path=None, url=None, overwrite: bool = False):
+    if path is None:
+        path = 'queries.txt'
+
+    path = Path(path).expanduser()
+
+    if overwrite and url is not None:
+        if path.is_file():
+            print(f"Downloading '{url}' and overwriting to '{path}'...")
+        else:
+            print(f"Downloading '{url}' to '{path}'...")
+        file_path, _ = urllib.urlretrieve(url=url, filename=path)
+        return path
+
+    if path.is_file():
+        print(f'using existing file: "{path.absolute()}"')
+        return path
+
+    _path = get_resource(str(path))
+    if not _path.is_file():
+        assert url is not None, "you must provide a valid URL to download the queries"
+        path = Path.cwd() / path
+        print(f"Downloading '{url}' to '{path}'...")
+        file_path, _ = urllib.urlretrieve(url=url, filename=path)
+        return path
+    else:
+        print(f"using package's default test file \"{_path}\"")
+        path = _path
+        return path
